f39660fbce3184b46ee6165b3abc7354616c20ef
Andre Pitombeira <andrepitombeira@gmail.com>
2016-08-02 12:39:59 +0200

    Add css loader

--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,10 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader'
       },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      },
     ],
   },
 };
